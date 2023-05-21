85696a4b40326d6ab87ab8564e1ee1bb13608f51
warbyd <dalewwarburton@gmail.com>
2023-05-21 12:46:21 +0100

    updated no file open page

--- src/NoFilesOpen.js
+++ src/NoFilesOpen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FiFile } from 'react-icons/fi';
 import './NoFilesOpen.css';
 import Logo from './logo.png'; // Path to your grayscale logo
 
@@ -7,8 +6,6 @@ const NoFilesOpen = () => {
   return (
     <div className="noFilesOpen">
       <div className="logoDiv" style={{ backgroundImage: `url(${Logo})` }}></div>
-      <FiFile size={80} className="fileLogo" />
-      <p>No files open</p>
     </div>
   );
 };
